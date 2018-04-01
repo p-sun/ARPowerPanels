55ca8ac216325bf32b45b59baf6a59ac7da3f30f
Paige Sun <psun@tribalscale.com>
2018-04-01 15:56:11 -0400

    Start a playground book, and rename to put source files in one folder

--- ARPowerPanelsDemo/ARPowerPanels/View Controllers/GlobalPlayground.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GlobalPlayground.swift
-//  ARPowerPanels
-//
-//  Created by TSD040 on 2018-04-01.
-//  Copyright © 2018 Pei Sun. All rights reserved.
-//
-
-import Foundation
---
+++ ARPowerPanelsDemo/ARPowerPanelsDemo/Source/View Controllers/GlobalPlayground.swift
@@ -0,0 +1,24 @@
+//
+//  GlobalPlayground.swift
+//  ARPowerPanels
+//
+//  Created by TSD040 on 2018-04-01.
+//  Copyright © 2018 Pei Sun. All rights reserved.
+//
+
+// Global Methods used in the playground
+
+import UIKit
+import ARKit
+
+func detectedPlane(plane: ARPlaneAnchor) {
+    //#-copy-source(id1)
+    //#-editable-code Remember to call the playSound(_:) function here!
+    
+    //#-end-editable-code
+    //#-end-copy-source
+    
+    //#-hidden-code
+
+    //#-end-hidden-code
+}
