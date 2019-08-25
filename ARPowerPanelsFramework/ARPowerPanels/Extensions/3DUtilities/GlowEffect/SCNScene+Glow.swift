93d427fb70efd1a954da161d70dc0f4277796c6c
Paige Sun <paige.sun.dev@gmail.com>
2019-08-25 16:44:56 -0400

    feat: Visualize selected nodes with an emission material

--- ARPowerPanelsFramework/ARPowerPanels/Extensions/3DUtilities/GlowEffect/SCNScene+Glow.swift
+++ ARPowerPanelsFramework/ARPowerPanels/Extensions/3DUtilities/GlowEffect/SCNScene+Glow.swift
@@ -8,36 +8,45 @@
 
 import SceneKit
 
-let glowMaterial: SCNMaterial = {
-    let mat = SCNMaterial()
-    mat.emission.contents = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
-    mat.name = "Glow Material"
-    return mat
-}()
+struct GlowNodes {
+    static let glowMaterial: SCNMaterial = {
+        let mat = SCNMaterial()
+        mat.emission.contents = UIColor.red.withAlphaComponent(0.3)
+        mat.name = "Glow Material"
+        return mat
+    }()
+}
 
 extension SCNNode {
     func setGlow(_ shouldGlow: Bool) {
-
-        func setGlow(on node: SCNNode) {
+        func setGlowMaterial(on node: SCNNode) -> Bool {
             if shouldGlow {
                 if let allMaterials = node.geometry?.materials {
-                    if !allMaterials.contains(glowMaterial) {
-                        node.geometry?.materials = [glowMaterial] + allMaterials
+                    if !allMaterials.contains(GlowNodes.glowMaterial) {
+                        node.geometry?.materials = [GlowNodes.glowMaterial] + allMaterials
+                        return true
                     }
                 }
             } else {
-                if let glowindex = node.geometry?.materials.index(of: glowMaterial) {
+                if let glowindex = node.geometry?.materials.index(of: GlowNodes.glowMaterial) {
                     node.geometry?.materials.remove(at: glowindex)
+                    return true
                 }
+            }
+            return false
+        }
+
+        if name == TransformationType.boundingBox.displayName {
+            return
+        }
+        
+        if !setGlowMaterial(on: self) {
+            for child in childNodes {
+                child.setGlow(shouldGlow)
             }
         }
         
-        setGlow(on: self)
-        enumerateHierarchy({ (node, _) in
-           setGlow(on: node)
-        })
-        
-        // For metal shader if needed
+        // For metal shader
 //        categoryBitMask = shouldGlow ? 2 : 1
 //        enumerateHierarchy({ (node, _) in
 //            node.categoryBitMask = shouldGlow ? 2 : 1
@@ -45,15 +54,16 @@ extension SCNNode {
     }
 }
 
-extension SCNView {
-    public func setupGlowTechnique() {
-        let bundle = Bundle(for: ModelCollectionView.self)
-        if let path = bundle.path(forResource: "NodeTechnique", ofType: "plist") {
-            if let dict = NSDictionary(contentsOfFile: path)  {
-                let dict2 = dict as! [String : AnyObject]
-                let technique = SCNTechnique(dictionary:dict2)
-                self.technique = technique
-            }
-        }
-    }
-}
+// Initialize Metal Shader
+//extension SCNView {
+//    public func setupGlowTechnique() {
+//        let bundle = Bundle(for: ModelCollectionView.self)
+//        if let path = bundle.path(forResource: "NodeTechnique", ofType: "plist") {
+//            if let dict = NSDictionary(contentsOfFile: path)  {
+//                let dict2 = dict as! [String : AnyObject]
+//                let technique = SCNTechnique(dictionary:dict2)
+//                self.technique = technique
+//            }
+//        }
+//    }
+//}
